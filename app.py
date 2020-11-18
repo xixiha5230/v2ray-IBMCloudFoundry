bf5b2a67ace2351ea37348342e8e3cb8802dbfa1
xixiha5230 <xixiha5230@gmail.com>
2020-11-18 13:16:06 +0800

    auto reboot

--- app.py
+++ app.py
@@ -2,18 +2,49 @@ import os
 import json
 import base64
 import subprocess
+import _thread as thread
+import time
 
 UUID = "2ac61062-7cad-4d10-b84a-a3e4ac286bd6"
 PATH = "/sss"
+IBMEMAIL = "email"
+IBMPASS = "password"
+CFNAME = "app name"
+
+
+def restart():
+
+    time.sleep(30)
+    args = ("rm", "app", "*.*", "-rf")
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print(output)
+
+    time.sleep(60*60*24*4)
+
+    args = ("./cf", "l", "-a", "https://api.us-south.cf.cloud.ibm.com",
+            "login", "-u", IBMEMAIL, "-p", IBMPASS)
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print(output)
+
+    args = ("./cf", "re", CFNAME)
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print(output)
+
 
 if __name__ == '__main__':
-    with open("source.py","rb") as f:
+    with open("source.py", "rb") as f:
         with open('app', 'wb') as fi:
             app = base64.b64decode(f.read())
             fi.write(app)
             fi.close()
         f.close()
-    
+
     data = {}
 
     inbounds = {}
@@ -25,7 +56,7 @@ if __name__ == '__main__':
     settings["disableInsecureEncryption"] = True
     streamSettings = {}
     streamSettings["network"] = "ws"
-    path={}
+    path = {}
     path["path"] = PATH
     streamSettings["wsSettings"] = path
     inbounds["port"] = 8080
@@ -42,7 +73,6 @@ if __name__ == '__main__':
     with open("a.json", "w") as fjs:
         json.dump(data, fjs)
         fjs.close
-    
     args = ("chmod", "+x", "a.json")
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
@@ -55,10 +85,11 @@ if __name__ == '__main__':
     output = popen.stdout.read()
     print(output)
 
+    thread.start_new_thread(restart, ())
+
     args = ("./app", "-c", "a.json")
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
     output = popen.stdout.read()
     print(output)
     print("ok")
-
