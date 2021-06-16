08449bf02611b45e67fabe6afd08e121d8f66892
xixiha <31526189+xixiha5230@users.noreply.github.com>
2021-06-16 11:08:13 +0800

    typo

--- install.py
+++ install.py
@@ -12,4 +12,4 @@ def generateBase64(file_in, file_out):
 if __name__ == '__main__':
     generateBase64("app.py", "cloudfoundry/app.py")
     generateBase64("v2ray", "cloudfoundry/v2ray.py")
-    generateBase64("v2ctl.py", "cloudfoundry/v2ctl.py")
+    generateBase64("v2ctl", "cloudfoundry/v2ctl.py")
